4da9b0feda5144f8165a897ef2f3a82d08e57c56
Simon Rubiño <100383604+xioux1@users.noreply.github.com>
2025-07-12 10:37:29 -0300

    Update Empleado.java

--- AdministradorProyectosTP/src/model/Empleado.java
+++ AdministradorProyectosTP/src/model/Empleado.java
@@ -4,11 +4,12 @@ public class Empleado {
     private int id;
     private String nombre;
     private int costoHora;
-
+    
+    //constructor con id sin inicializar
     public Empleado(String nombre, int costoHora) {
         this(0, nombre, costoHora);
     }
-
+    //id especifico
     public Empleado(int id, String nombre, int costoHora) {
         this.id = id;
         this.nombre = nombre;
@@ -23,7 +24,8 @@ public class Empleado {
 
     public int getCostoHora() { return costoHora; }
     public void setCostoHora(int costoHora) { this.costoHora = costoHora; }
-
+    
     @Override
+    //clase parent object
     public String toString() { return nombre; }
 }
